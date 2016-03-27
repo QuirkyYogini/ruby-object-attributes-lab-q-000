94902c41a73dd5104c0ab8a80264259df52a9d24
QuirkyYogini <quirkyyogini@gmail.com>
2016-03-27 16:08:27 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  def name=(person_name)
+    @name = person_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(jobby_job)
+    @job = jobby_job
+  end
+
+  def job
+    @job
+  end
+end
+
